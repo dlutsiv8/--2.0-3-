496e5e7e0eff4be27191098b5ee96a7e14590ec8
dlutsiv8 <dlutsiv8@gmail.com>
2025-09-21 22:34:06 +0300

    завдання3

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -19,8 +19,8 @@ int main()
     cout << "z2 = " << z2 << endl;
 
     return 0;
-    // комент
-    // лаба 2.0
+    // pdf
+    
     
 }          
     
@@ -36,4 +36,4 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-Pr2 змінна 2
+
